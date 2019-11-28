cddbed6f44462e8a848f1808e4dac14d4b468366
Hugo Wood <hx4.5@free.fr>
2019-11-28 23:16:45 +0100

    ♻️ inline alibeez request options

--- multibeez.js
+++ multibeez.js
@@ -11,25 +11,26 @@ const ALIBEEZ_API_BASE_URL =
 const ALIBEEZ_API_BASE_URL_PARSED = url.parse(ALIBEEZ_API_BASE_URL);
 
 const server = http.createServer((req, res) => {
-  const options = {
-    hostname: ALIBEEZ_API_BASE_URL_PARSED.host,
-    port: ALIBEEZ_API_BASE_URL_PARSED.port,
-    path: ALIBEEZ_API_BASE_URL_PARSED.path + req.url,
-    method: "GET",
-    headers: {
-      ...req.headers,
-      host: ALIBEEZ_API_BASE_URL_PARSED.host
+  const alibeezRequest = https.request(
+    {
+      hostname: ALIBEEZ_API_BASE_URL_PARSED.host,
+      port: ALIBEEZ_API_BASE_URL_PARSED.port,
+      path: ALIBEEZ_API_BASE_URL_PARSED.path + req.url,
+      method: "GET",
+      headers: {
+        ...req.headers,
+        host: ALIBEEZ_API_BASE_URL_PARSED.host
+      }
+    },
+    alibeezResponse => {
+      res.writeHead(
+        alibeezResponse.statusCode,
+        alibeezResponse.statusMessage,
+        alibeezResponse.headers
+      );
+      alibeezResponse.pipe(res);
     }
-  };
-
-  const alibeezRequest = https.request(options, alibeezResponse => {
-    res.writeHead(
-      alibeezResponse.statusCode,
-      alibeezResponse.statusMessage,
-      alibeezResponse.headers
-    );
-    alibeezResponse.pipe(res);
-  });
+  );
 
   alibeezRequest.on("error", err => {
     console.error(err);
