44eac3625c19656928d15866c8c1f19780ce10e2
Hugo Wood <hx4.5@free.fr>
2019-11-29 00:42:35 +0100

    ♻️ extract functions

--- multibeez.js
+++ multibeez.js
@@ -5,74 +5,90 @@ const fetch = require("node-fetch");
 const querystring = require("querystring");
 const url = require("url");
 
-const PORT = process.env.PORT || 4000;
-const ALIBEEZ_API_BASE_URL =
-  process.env.ALIBEEZ_API_BASE_URL ||
-  "https://dev-infra.my.alibeez.com/api/query";
-const ALIBEEZ_API_BASE_URL_PARSED = url.parse(ALIBEEZ_API_BASE_URL);
+const startServer = (port, alibeezBaseUrl) => {
+  return http
+    .createServer(async (req, res) => {
+      const alibeezKeys = readAlibeezKeys(req);
+      if (!alibeezKeys) {
+        res.writeHead(401);
+        res.end();
+        return;
+      }
+      try {
+        const bodies = await Promise.all(
+          alibeezKeys.map(async key => {
+            return requestAlibeez(
+              buildAlibeezUrl(alibeezBaseUrl, req.url, key)
+            );
+          })
+        );
+        res.writeHead(200);
+        res.end(JSON.stringify(bodies));
+      } catch (err) {
+        console.error(err);
+        res.writeHead(err.status, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(err));
+        return;
+      }
+    })
+    .listen(port, () => {
+      console.log(`listening on ${port}`);
+    });
+};
 
-const server = http.createServer(async (req, res) => {
-  const [, token] =
-    (req.headers.authorization || "").match(/Bearer (.*)/) || [];
+const readAlibeezKeys = req => {
+  const authorizationHeader = req.headers.authorization;
+  if (!authorizationHeader) {
+    return;
+  }
+  const [, token] = authorizationHeader.match(/Bearer (.*)/) || [];
   if (!token) {
-    res.writeHead(401);
-    res.end();
     return;
   }
   const alibeezKeys = token.split(",").filter(key => key);
   if (alibeezKeys.length === 0) {
-    res.writeHead(401);
-    res.end();
     return;
   }
+  return alibeezKeys;
+};
 
-  try {
-    const bodies = await Promise.all(
-      alibeezKeys.map(async key => {
-        const reqUrlParsed = url.parse(req.url);
-        const alibeezUrl = url.format({
-          ...ALIBEEZ_API_BASE_URL_PARSED,
-          pathname: ALIBEEZ_API_BASE_URL_PARSED.path + reqUrlParsed.pathname,
-          search: querystring.stringify({
-            ...querystring.parse(reqUrlParsed.search.replace(/^\?/, "")),
-            ...querystring.parse(ALIBEEZ_API_BASE_URL_PARSED.search),
-            key
-          })
-        });
-        const response = await fetch(alibeezUrl, {
-          method: "GET",
-          headers: {
-            ...req.headers,
-            host: ALIBEEZ_API_BASE_URL_PARSED.host
-          }
-        });
-        const isJson = response.headers
-          .get("Content-Type")
-          .startsWith("application/json");
-        const body = isJson ? await response.json() : null;
-        if (!response.ok) {
-          throw {
-            status: 504,
-            error: {
-              status: response.status,
-              statusText: response.statusText,
-              body
-            }
-          };
-        }
-        return body;
-      })
-    );
-    res.writeHead(200);
-    res.end(JSON.stringify(bodies));
-  } catch (err) {
-    console.error(err);
-    res.writeHead(err.status, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(err));
-    return;
+const buildAlibeezUrl = (alibeezBaseUrl, requestedUrl, alibeezKey) => {
+  const requestedUrlParsed = url.parse(requestedUrl);
+  const alibeezBaseUrlParsed = url.parse(alibeezBaseUrl);
+  return url.format({
+    ...alibeezBaseUrlParsed,
+    pathname: alibeezBaseUrlParsed.path + requestedUrlParsed.pathname,
+    search: querystring.stringify({
+      ...querystring.parse(requestedUrlParsed.search.replace(/^\?/, "")),
+      ...querystring.parse(alibeezBaseUrlParsed.search),
+      key: alibeezKey
+    })
+  });
+};
+
+const requestAlibeez = async url => {
+  const response = await fetch(url, {
+    method: "GET"
+  });
+  const isJson = response.headers
+    .get("Content-Type")
+    .startsWith("application/json");
+  const body = isJson ? await response.json() : null;
+  if (!response.ok) {
+    throw {
+      status: 504,
+      error: {
+        status: response.status,
+        statusText: response.statusText,
+        body
+      }
+    };
   }
-});
+  return body;
+};
 
-server.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
+startServer(
+  process.env.PORT || 4000,
+  process.env.ALIBEEZ_API_BASE_URL ||
+    "https://dev-infra.my.alibeez.com/api/query"
+);
