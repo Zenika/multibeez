538cdf609ee6dac216f9ece8962386a571e15df9
Hugo Wood <hx4.5@free.fr>
2019-11-29 00:32:39 +0100

    ✨ support multiple alibeez instances

--- multibeez.js
+++ multibeez.js
@@ -1,7 +1,8 @@
 // @ts-check
 
 const http = require("http");
-const https = require("https");
+const fetch = require("node-fetch");
+const querystring = require("querystring");
 const url = require("url");
 
 const PORT = process.env.PORT || 4000;
@@ -10,35 +11,66 @@ const ALIBEEZ_API_BASE_URL =
   "https://dev-infra.my.alibeez.com/api/query";
 const ALIBEEZ_API_BASE_URL_PARSED = url.parse(ALIBEEZ_API_BASE_URL);
 
-const server = http.createServer((req, res) => {
-  const alibeezRequest = https.request(
-    {
-      hostname: ALIBEEZ_API_BASE_URL_PARSED.host,
-      port: ALIBEEZ_API_BASE_URL_PARSED.port,
-      path: ALIBEEZ_API_BASE_URL_PARSED.path + req.url,
-      method: "GET",
-      headers: {
-        ...req.headers,
-        host: ALIBEEZ_API_BASE_URL_PARSED.host
-      }
-    },
-    alibeezResponse => {
-      res.writeHead(
-        alibeezResponse.statusCode,
-        alibeezResponse.statusMessage,
-        alibeezResponse.headers
-      );
-      alibeezResponse.pipe(res);
-    }
-  );
-
-  alibeezRequest.on("error", err => {
-    console.error(err);
-    res.writeHead(504);
+const server = http.createServer(async (req, res) => {
+  const [, token] =
+    (req.headers.authorization || "").match(/Bearer (.*)/) || [];
+  if (!token) {
+    res.writeHead(401);
     res.end();
-  });
+    return;
+  }
+  const alibeezKeys = token.split(",").filter(key => key);
+  if (alibeezKeys.length === 0) {
+    res.writeHead(401);
+    res.end();
+    return;
+  }
 
-  req.pipe(alibeezRequest);
+  try {
+    const bodies = await Promise.all(
+      alibeezKeys.map(async key => {
+        const reqUrlParsed = url.parse(req.url);
+        const alibeezUrl = url.format({
+          ...ALIBEEZ_API_BASE_URL_PARSED,
+          pathname: ALIBEEZ_API_BASE_URL_PARSED.path + reqUrlParsed.pathname,
+          search: querystring.stringify({
+            ...querystring.parse(reqUrlParsed.search.replace(/^\?/, "")),
+            ...querystring.parse(ALIBEEZ_API_BASE_URL_PARSED.search),
+            key
+          })
+        });
+        const response = await fetch(alibeezUrl, {
+          method: "GET",
+          headers: {
+            ...req.headers,
+            host: ALIBEEZ_API_BASE_URL_PARSED.host
+          }
+        });
+        const isJson = response.headers
+          .get("Content-Type")
+          .startsWith("application/json");
+        const body = isJson ? await response.json() : null;
+        if (!response.ok) {
+          throw {
+            status: 504,
+            error: {
+              status: response.status,
+              statusText: response.statusText,
+              body
+            }
+          };
+        }
+        return body;
+      })
+    );
+    res.writeHead(200);
+    res.end(JSON.stringify(bodies));
+  } catch (err) {
+    console.error(err);
+    res.writeHead(err.status, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(err));
+    return;
+  }
 });
 
 server.listen(PORT, () => {
